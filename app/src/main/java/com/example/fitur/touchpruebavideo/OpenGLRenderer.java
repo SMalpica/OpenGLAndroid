4d4bc8726f06261c99f0452bcf804bfec73527c5
Sandra Malpica <sandra_malpica@hotmail.com>
2015-07-09 17:42:40 +0200

    intersection test for blue mallet

--- app/src/main/java/com/example/fitur/touchpruebavideo/OpenGLRenderer.java
+++ app/src/main/java/com/example/fitur/touchpruebavideo/OpenGLRenderer.java
@@ -35,14 +35,21 @@ import static android.opengl.Matrix.*;
  */
 public class OpenGLRenderer implements GLSurfaceView.Renderer{
     private final Context context;
+
+    //matrix definitions
     private final float[] projectionMatrix = new float[16];
     private final float[] modelMatrix = new float[16];
     private final float[] viewMatrix = new float[16];
     private final float[] viewProjectionMatrix = new float[16];
     private final float[] modelViewProjectionMatrix = new float[16];
+    private final float[] invertedViewProjectionMatrix = new float[16];
+
+    //object definitions
     private Table table;
     private Mallet mallet;
     private Puck puck;
+
+    //program definitions
     private TextureShaderProgram textureProgram;
     private ColorShaderProgram colorProgram;
     private int texture;
@@ -129,6 +136,12 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer{
         // Clear the rendering surface.
         glClear(GL_COLOR_BUFFER_BIT);
         multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        /*This call will create an inverted matrix that we’ll be able to use to convert the
+        two-dimensional touch point into a pair of three-dimensional coordinates. If
+        we move around in our scene, it will affect which part of the scene is underneath
+        our fingers, so we also want to take the view matrix into account. We
+        do this by taking the inverse of the combined view and projection matrices.*/
+        invertM(invertedViewProjectionMatrix, 0, viewProjectionMatrix, 0);
         positionTableInScene();
         textureProgram.useProgram();
         textureProgram.setUniforms(modelViewProjectionMatrix, texture);
@@ -188,10 +201,10 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer{
 
     public void handleTouchPress(float normalizedX, float normalizedY) {
         Log.e("TOUCH_EVENT", "press in : " + normalizedX + ", " + normalizedY);
-        /*Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
+        Geometry.Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
         // Now test if this ray intersects with the mallet by creating a
         // bounding sphere that wraps the mallet.
-        Sphere malletBoundingSphere = new Sphere(new Geometry.Point(
+        Geometry.Sphere malletBoundingSphere = new Geometry.Sphere(new Geometry.Point(
                 blueMalletPosition.x,
                 blueMalletPosition.y,
                 blueMalletPosition.z),
@@ -199,10 +212,39 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer{
         // If the ray intersects (if the user touched a part of the screen that
         // intersects the mallet's bounding sphere), then set malletPressed =
         // true.
-        malletPressed = Geometry.intersects(malletBoundingSphere, ray);*/
+        malletPressed = Geometry.intersects(malletBoundingSphere, ray);
+        Log.e("MALLET_PRESSED","vale "+malletPressed);
     }
 
     public void handleTouchDrag(float normalizedX, float normalizedY) {
         Log.e("TOUCH_EVENT","dragged to : "+normalizedX+", "+normalizedY);
+    }
+
+    private Geometry.Ray convertNormalized2DPointToRay(float normalizedX, float normalizedY) {
+        // We'll convert these normalized device coordinates into world-space
+        // coordinates. We'll pick a point on the near and far planes, and draw a
+        // line between them. To do this transform, we need to first multiply by
+        // the inverse matrix, and then we need to undo the perspective divide.
+        final float[] nearPointNdc = {normalizedX, normalizedY, -1, 1};
+        final float[] farPointNdc = {normalizedX, normalizedY, 1, 1};
+        final float[] nearPointWorld = new float[4];
+        final float[] farPointWorld = new float[4];
+        multiplyMV(nearPointWorld, 0, invertedViewProjectionMatrix, 0, nearPointNdc, 0);
+        multiplyMV(farPointWorld, 0, invertedViewProjectionMatrix, 0, farPointNdc, 0);
+        divideByW(nearPointWorld);
+        divideByW(farPointWorld);
+
+        Geometry.Point nearPointRay =
+                new Geometry.Point(nearPointWorld[0], nearPointWorld[1], nearPointWorld[2]);
+        Geometry.Point farPointRay =
+                new Geometry.Point(farPointWorld[0], farPointWorld[1], farPointWorld[2]);
+        return new Geometry.Ray(nearPointRay,
+                Geometry.vectorBetween(nearPointRay, farPointRay));
+    }
+
+    private void divideByW(float[] vector) {
+        vector[0] /= vector[3];
+        vector[1] /= vector[3];
+        vector[2] /= vector[3];
     }
 }
--- app/src/main/java/com/example/fitur/util/Geometry.java
+++ app/src/main/java/com/example/fitur/util/Geometry.java
@@ -1,5 +1,7 @@
 package com.example.fitur.util;
 
+import android.util.FloatMath;
+
 /***
  * Excerpted from "OpenGL ES for Android",
  * published by The Pragmatic Bookshelf.
@@ -28,6 +30,12 @@ public class Geometry {
         public Point translateY(float distance) {
             return new Point(x, y + distance, z);
         }
+        public Point translate(Vector vector) {
+            return new Point(
+                    x + vector.x,
+                    y + vector.y,
+                    z + vector.z);
+        }
     }
 
     /*class to define a circle*/
@@ -54,5 +62,75 @@ public class Geometry {
             this.radius = radius;
             this.height = height;
         }
+    }
+
+    public static class Ray {
+        public final Point point;
+        public final Vector vector;
+        public Ray(Point point, Vector vector) {
+            this.point = point;
+            this.vector = vector;
+        }
+    }
+
+    public static class Vector {
+        public final float x, y, z;
+        public Vector(float x, float y, float z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        //returns the length of the vector applying pythagoras theorem
+        public float length() {
+            return FloatMath.sqrt(
+                    x * x
+                            + y * y
+                            + z * z);
+        }
+        // http://en.wikipedia.org/wiki/Cross_product
+        public Vector crossProduct(Vector other) {
+            return new Vector(
+                    (y * other.z) - (z * other.y),
+                    (z * other.x) - (x * other.z),
+                    (x * other.y) - (y * other.x));
+        }
+    }
+
+    public static Vector vectorBetween(Point from, Point to) {
+        return new Vector(
+                to.x - from.x,
+                to.y - from.y,
+                to.z - from.z);
+    }
+
+    public static class Sphere {
+        public final Point center;
+        public final float radius;
+        public Sphere(Point center, float radius) {
+            this.center = center;
+            this.radius = radius;
+        }
+    }
+
+    public static boolean intersects(Sphere sphere, Ray ray) {
+        return distanceBetween(sphere.center, ray) < sphere.radius;
+    }
+
+    public static float distanceBetween(Point point, Ray ray) {
+        //two vectors from 2 line points to the center of the sphere. they define a triangle
+        Vector p1ToPoint = vectorBetween(ray.point, point);
+        Vector p2ToPoint = vectorBetween(ray.point.translate(ray.vector), point);
+        // The length of the cross product gives the area of an imaginary
+        // parallelogram having the two vectors as sides. A parallelogram can be
+        // thought of as consisting of two triangles, so this is the same as
+        // twice the area of the triangle defined by the two vectors.
+        // http://en.wikipedia.org/wiki/Cross_product#Geometric_meaning
+        float areaOfTriangleTimesTwo = p1ToPoint.crossProduct(p2ToPoint).length();
+        float lengthOfBase = ray.vector.length();
+        // The area of a triangle is also equal to (base * height) / 2. In
+        // other words, the height is equal to (area * 2) / base. The height
+        // of this triangle is the distance from the point to the ray.
+        float distanceFromPointToRay = areaOfTriangleTimesTwo / lengthOfBase;
+        return distanceFromPointToRay;
     }
 }
